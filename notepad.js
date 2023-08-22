6bf288758739c2aac2ad82e794aa81364790269e
Vigneshwaran M <vignesh510510@gmail.com>
2023-08-22 13:11:57 +0530

    Update notepad.js

--- notepad.js
+++ notepad.js
@@ -3,3 +3,37 @@ var fileContent = document.getElementById("txtContent");
 
 fileName.value="";
 fileContent.value="";
+function downloadFile()
+{
+if(fileName.value === "" || fileContent.value ==="")
+window.alert("Please enter File name and content");
+else
+{
+var e = fileContent.value;
+var c = document.createElement("a");
+c.download = fileName.value + ".txt";
+var t = new Blob([e], {
+type: "text/plain"
+});
+c.href = window.URL.createObjectURL(t);
+c.click();
+}
+}
+
+function changeStyle(propertyName)
+{
+switch(propertyName)
+{
+case "b":
+if(fileContent.style.fontWeight === "bold")
+fileContent.style.fontWeight = "normal";
+else
+fileContent.style.fontWeight = "bold";
+break;
+case "i":
+if(fileContent.style.fontStyle === "italic")
+fileContent.style.fontStyle = "normal";
+else
+fileContent.style.fontStyle = "italic";
+break;
+case "n":
